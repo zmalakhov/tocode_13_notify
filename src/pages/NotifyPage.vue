e0fb47bc6b5b33cd418467ca04f8a7cca3f8a8d6
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-03 10:51:12 +0300

    #1 - Установка - Установка и базовая настройка

---
+++ src/pages/NotifyPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+        <section>
+            <div class="container">
+                <notify
+                    :messages="messages"
+                />
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    import notify from '@/components/Notify.vue'
+    export default {
+        components: { notify },
+        data(){
+            return{
+                messages: [
+                    { title: 'message 1' },
+                    { title: 'message 2' },
+                    { title: 'message 3' },
+                    { title: 'message 4' },
+                    { title: 'message 5' },
+                    { title: 'message 6' },
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
